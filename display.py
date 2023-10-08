e3dbe56162f04622d6aadbf7cc89ee6936e186d1
khushi kapoor <140336136+kapoorkhushi03@users.noreply.github.com>
2023-10-09 00:00:29 +0530

    Update display.py

--- display.py
+++ display.py
@@ -1,32 +1,40 @@
-#this is the display file
 import requests
 
-def get_exchange_rate(base_currency, target_currency):
-    # Replace with the URL of the currency exchange rate API
-    api_url = f"https://api.exchangeratesapi.io/latest?base={base_currency}"
-    
+# Define your API key from exchangeratesapi.io
+api_key = "YOUR_API_KEY_HERE"
+
+# Function to convert currency using the API
+def convert_currency(currency_name, currency_value, preferred_currency):
+    # Build the URL for the API request
+    base_url = "https://api.apilayer.com/exchangerates_data/latest"
+    params = {
+        "access_key": api_key,
+        "base": currency_name,
+        "symbols": preferred_currency
+    }
+
     try:
-        response = requests.get(api_url)
+        response = requests.get(base_url, params=params)
         data = response.json()
         
-        if target_currency in data["rates"]:
-            exchange_rate = data["rates"][target_currency]
-            return exchange_rate
+        if response.status_code == 200 and 'rates' in data:
+            exchange_rate = data['rates'][preferred_currency]
+            converted_value = currency_value * exchange_rate
+            return converted_value
         else:
-            return None
-        
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching exchange rate: {e}")
-        return None
+            return "Error: Unable to fetch exchange rates from the API"
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
 
-# Example usage:
-if __name__ == "__main__":
-    base_currency = "USD"
-    target_currency = "EUR"
-    
-    exchange_rate = get_exchange_rate(base_currency, target_currency)
-    
-    if exchange_rate is not None:
-        print(f"Exchange rate from {base_currency} to {target_currency}: {exchange_rate}")
-    else:
-        print(f"Unable to fetch the exchange rate for {base_currency} to {target_currency}")
+# Input currency name, value, and preferred currency
+currency_name = input("Enter the currency name (e.g., USD, EUR, GBP): ").upper()
+currency_value = float(input("Enter the currency value: "))
+preferred_currency = input("Enter the preferred currency (e.g., USD, EUR, GBP): ").upper()
+
+# Convert and display the result
+converted_value = convert_currency(currency_name, currency_value, preferred_currency)
+if isinstance(converted_value, str):
+    print(converted_value)
+else:
+    print(f"{currency_value} {currency_name} is equivalent to {converted_value:.2f} {preferred_currency}")
+
