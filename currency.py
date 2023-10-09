c383d72ddeede70bcd34f60a97842b4ac06049df
Ekansh <ekanshbhushan2k22@gmail.com>
2023-10-09 21:09:39 +0530

    functions do requests created

--- currency.py
+++ currency.py
@@ -1,1 +1,19 @@
 import requests
+from datetime import datetime
+
+class Currency:
+    def __init__(self):
+        self.api_key = open('api_key.txt').readline().strip()
+        self.url = f'http://api.exchangeratesapi.io/v1/latest?access_key={self.api_key}'
+        self.output = ''
+        self.file_name = datetime.now().strftime('%d %b - %Y')
+        print(self.file_name)
+
+    def do_request(self):
+        res = requests.get(self.url)
+        if res.status_code == 200:
+            self.output = res.json()
+            print(self.output)
+
+c = Currency()
+c.do_request()
