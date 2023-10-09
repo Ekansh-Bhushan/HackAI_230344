879d2a3f1c14a72857b22b65d183bf051441b647
Ekansh <ekanshbhushan2k22@gmail.com>
2023-10-09 21:16:06 +0530

    API connected

--- currency.py
+++ currency.py
@@ -5,6 +5,7 @@ class Currency:
     def __init__(self):
         self.api_key = open('api_key.txt').readline().strip()
         self.url = f'http://api.exchangeratesapi.io/v1/latest?access_key={self.api_key}'
+        # self.url = f'http://api.exchangeratesapi.io/v1/latest?access_key={self.api_key}&base=USD&symbols=GBP,EUR'
         self.output = ''
         self.file_name = datetime.now().strftime('%d %b - %Y')
         print(self.file_name)
@@ -13,7 +14,9 @@ class Currency:
         res = requests.get(self.url)
         if res.status_code == 200:
             self.output = res.json()
+            # print(self.output['rates']['GBP'])
             print(self.output)
+
 
 c = Currency()
 c.do_request()
