a895d226219e01c999185d6d295cacc8ecde1342
Ekansh <ekanshbhushan2k22@gmail.com>
2023-10-11 02:21:15 +0530

    email.py

--- src/agents/currency/currencyrates.py
+++ src/agents/currency/currencyrates.py
@@ -5,32 +5,32 @@ from uagents import Agent, Context
 
 # from uagents import Agent, Context
 
-class Currency:
-    def __init__(self):
-        self.api_key = open('api_key.txt').readline().strip()
-        self.url = f'http://api.exchangeratesapi.io/v1/latest?access_key={self.api_key}'
-        # self.url = f'http://api.exchangeratesapi.io/v1/latest?access_key={self.api_key}&base=USD&symbols=GBP,EUR'
-        self.output = ''
-        self.file_name = datetime.now().strftime('%d %b - %Y')
-        print(self.file_name)
 
-    def do_request(self):
-        res = requests.get(self.url)
-        if res.status_code == 200:
-            res_json = res.json()
-            self.output = res_json['rates']['USD']
+def __init__(self):
+    self.api_key = open('api_key.txt').readline().strip()
+    self.url = f'http://api.exchangeratesapi.io/v1/latest?access_key={self.api_key}'
+    # self.url = f'http://api.exchangeratesapi.io/v1/latest?access_key={self.api_key}&base=USD&symbols=GBP,EUR'
+    self.output = ''
+    self.file_name = datetime.now().strftime('%d %b - %Y')
+    print(self.file_name)
 
-    agent= Agent(name="agent", seed="agent recovery phase")
-    @agent.on_interval(period=84600)
-    async def currency_update(ctx: Context):
-        ctx.logger.info(f'The current rate today is {self.output}')
-    def write_to_file(self):
-        tday = datetime.now().strftime('%Y-%m-%d')
-        # print(tday)
-        temp_dict = {tday: self.output}
-        with open(f"daily_price/{self.file_name}.txt", 'w') as f :
-            json.dump(temp_dict, f)
-        # print(self)
+def do_request(self):
+    res = requests.get(self.url)
+    if res.status_code == 200:
+        res_json = res.json()
+        self.output = res_json['rates']['USD']
+
+agent= Agent(name="agent", seed="agent recovery phase")
+@agent.on_interval(period=84600)
+async def currency_update(ctx: Context):
+    ctx.logger.info(f'The current rate today is {self.output}')
+def write_to_file(self):
+    tday = datetime.now().strftime('%Y-%m-%d')
+    # print(tday)
+    temp_dict = {tday: self.output}
+    with open(f"daily_price/{self.file_name}.txt", 'w') as f :
+        json.dump(temp_dict, f)
+    # print(self)
 
     
 agent.run()
