e07b853ed4a1a183f537e96823160763651f7a62
Ekansh <ekanshbhushan2k22@gmail.com>
2023-10-11 03:42:47 +0530

    major changes

--- main.py
+++
@@ -1,7 +0,0 @@
-from uagents import Agent, Context
-agent = Agent(name="agent", seed="agent recovery phase")
-@agent.on_interval(period=60)
-async def currency_update(ctx:Context):
-    ctx.logger.info(f'The current rate today is {self.output}')
-
-agent.run()
